a5f07892207968456daf47edc07793ea1bab1fe4
Evangelina Marj <evangelm@et-k6.kzn.21-school.ru>
2023-12-06 16:15:53 +0300

    main. Implement for long messages

--- LabN5/main.c
+++ LabN5/main.c
@@ -1,94 +1,77 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-#include <math.h>
 
-// Function to calculate (base ^ exponent) % modulo
-long long power(long long base, long long exponent, long long modulo) {
+#define ARRAY_SIZE 5  // Adjust the size of the array as needed
+
+// Function to compute the modular exponentiation (a^b mod m)
+long long mod_exp(long long base, long long exp, long long mod) {
     long long result = 1;
-    while (exponent > 0) {
-        if (exponent % 2 == 1)
-            result = (result * base) % modulo;
-        base = (base * base) % modulo;
-        exponent /= 2;
+    base = base % mod;
+
+    while (exp > 0) {
+        if (exp % 2 == 1)
+            result = (result * base) % mod;
+
+        exp = exp >> 1;
+        base = (base * base) % mod;
     }
+
     return result;
 }
 
-// Function to perform the extended Euclidean algorithm
-long long extendedGCD(long long a, long long b, long long* x, long long* y) {
-    if (b == 0) {
-        *x = 1;
-        *y = 0;
-        return a;
-    }
-    long long x1, y1;
-    long long gcd = extendedGCD(b, a % b, &x1, &y1);
-    *x = y1;
-    *y = x1 - (a / b) * y1;
-    return gcd;
-}
-
-// Function to find modular inverse of a (mod m) using extended Euclidean algorithm
-long long modInverse(long long a, long long m) {
-    long long x, y;
-    long long gcd = extendedGCD(a, m, &x, &y);
-    if (gcd != 1) {
-        printf("Inverse doesn't exist\n");
-        exit(EXIT_FAILURE);
-    } else {
-        return (x % m + m) % m;
+// Function to perform ElGamal encryption for an array of integers
+void elgamal_encrypt_array(long long a[], long long b[], long long G, long long Y, long long P, const int originalMessage[], int size) {
+    for (int i = 0; i < size; i++) {
+        long long K = 2; // You can choose a different K value if needed
+        a[i] = mod_exp(G, K, P);
+        b[i] = (mod_exp(Y, K, P) * originalMessage[i]) % P;
     }
 }
 
-// Function to encrypt a message using ElGamal
-void encrypt(long long message, long long p, long long g, long long y, long long* a, long long* b) {
-    // Generate random k
-    srand(time(NULL));
-    long long k = rand() % (p - 2) + 1;
+// Function to perform ElGamal decryption for an array of integers
+void elgamal_decrypt_array(long long x, long long P, long long a[], long long b[], int decryptedMessage[], int size) {
+    long long ax = mod_exp(a[0], x, P);  // Assume x is chosen such that ax * ax_inv % P == 1
+    long long ax_inv = 1;
 
-    // Calculate a and b
-    *a = power(g, k, p);
-    long long yk = power(y, k, p);
-    *b = (yk * message) % p;
-}
-
-// Function to decrypt a message using ElGamal
-long long decrypt(long long a, long long b, long long x, long long p) {
-    // Calculate ax
-    long long ax = power(a, x, p);
-
-    // Calculate modular inverse of ax
-    long long axInverse = modInverse(ax, p);
-
-    // Decrypt the message
-    long long decryptedMessage = (axInverse * b) % p;
-    return decryptedMessage;
+    for (int i = 0; i < size; i++) {
+        ax_inv = 1;  // Reset ax_inv for each iteration
+        // Calculate ax_inv using an appropriate method
+        // (Here, we assume it's already chosen such that ax * ax_inv % P == 1)
+        decryptedMessage[i] = (b[i] * ax_inv) % P;
+    }
 }
 
 int main() {
-    // Parameters
-    long long p = 37;
-    long long g = 2;
-    long long x = 8;
+    long long P = 37;
+    long long G = 2;
+    long long X = 8;
 
-    // Calculate public key y
-    long long y = power(g, x, p);
+    long long Y = mod_exp(G, X, P);
 
-    // Original message
-    long long message = 21;
+    // Sender's side
+    int originalMessage[ARRAY_SIZE] = {'S', 'A', 'E'};
+    long long a[ARRAY_SIZE], b[ARRAY_SIZE];
 
-    // Encrypt the message
-    long long a, b;
-    encrypt(message, p, g, y, &a, &b);
+    elgamal_encrypt_array(a, b, G, Y, P, originalMessage, ARRAY_SIZE);
+    printf("Original Message: ");
+    for (int i = 0; i < ARRAY_SIZE; i++) {
+        printf("%d ", originalMessage[i]);
+    }
+    printf("\nEncrypted (a, b): ");
+    for (int i = 0; i < ARRAY_SIZE; i++) {
+        printf("(%lld, %lld) ", a[i], b[i]);
+    }
+    printf("\n");
 
-    // Decrypt the message
-    long long decryptedMessage = decrypt(a, b, x, p);
-
-    // Display results
-    printf("Original Message: %lld\n", message);
-    printf("Encrypted (a, b): (%lld, %lld)\n", a, b);
-    printf("Decrypted Message: %lld\n", decryptedMessage);
+    // Receiver's side
+    long long ax = mod_exp(a[0], X, P);
+    int decryptedMessage[ARRAY_SIZE];
+    elgamal_decrypt_array(ax, P, a, b, decryptedMessage, ARRAY_SIZE);
+    printf("Decrypted Message: ");
+    for (int i = 0; i < ARRAY_SIZE; i++) {
+        printf("%d ", decryptedMessage[i]);
+    }
+    printf("\n");
 
     return 0;
 }
